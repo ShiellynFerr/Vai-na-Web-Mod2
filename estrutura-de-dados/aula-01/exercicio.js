99005037a74b054bee3f131b35205ff7ff799a4b
ShiellynFerr <brunashiellyn@gmail.com>
2022-06-09 14:59:04 -0300

    exercicio js

---
+++ estrutura-de-dados/aula-01/exercicio.js
@@ -0,0 +1,28 @@
+// Crie uma variável que receba um nome;
+var nome = 'Shiellyn Bruna Ferreira';
+
+// Crie uma variável que receba um número;
+
+var idade = 20;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+var anoAtual = 2022;
+var subtracao = 2022 - 20;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+var resultado = anoAtual + subtracao;
+console.log(resultado);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log('Estou mexendo no console.log')
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+var quartaFeira = true;
+console.log(quartaFeira);
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira);
