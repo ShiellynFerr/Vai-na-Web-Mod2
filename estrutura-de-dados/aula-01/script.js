9d1bd4a6a80e5af9b14c76ebed524c05a8dbec92
ShiellynFerr <brunashiellyn@gmail.com>
2022-06-08 22:56:13 -0300

    estrutura de dados

---
+++ estrutura-de-dados/aula-01/script.js
@@ -0,0 +1,33 @@
+console.log('olá mundo');
+
+let texto =  'esse texto é uma string';
+
+
+let nome = 'Shiellyn';
+let sobrenome = 'Ferreira';
+
+let templateString = `Olá meu nome é ${nome} e meu sobrenome é ${sobrenome}`;
+
+console.log(nome, sobrenome);
+console.log(templateString);
+
+let boleano = true // dados boleanos, tipo verdadeiro ou falso;
+
+// Crie uma variável que receba um nome;
+let name = "Qualquer Nome"
+// Crie uma variável que receba um número;
+let numero = 85
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022 - numero
+console.log(ano)
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(numero+ano)
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log")
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa
+// verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+console.log(quartaFeira)
+//entrada e saída, entrada quartaFeira, saída true
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
