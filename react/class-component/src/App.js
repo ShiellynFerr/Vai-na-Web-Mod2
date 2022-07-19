62f7e9c3ebdf3bc626cc2177da9f55fa5136d4ca
ShiellynFerr <brunashiellyn@gmail.com>
2022-07-19 10:42:51 -0300

    class component react

---
+++ react/class-component/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Eu estou gostando muito de aprender react!</h1>
+      </div>
+    );
+  }
+}
